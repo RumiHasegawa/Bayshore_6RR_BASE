774810826dc8a25dcbf4c3bc54cf8cfe97c714b6
Rumi Hasegawa <nakayamaken824@gmail.com>
2026-08-15 20:17:37 +0800

    Update startup.ts

--- src/modules/startup.ts
+++ src/modules/startup.ts
@@ -148,6 +148,10 @@ export default class StartupModule extends Module {
                 expeditionSchedule: vsOrgEventDate || null,
                 expeditionEventWasHeld: true,
 
+                // Koshien
+                koshienEventWasHeld: false,
+                previousKoshienId: 0,
+
                 // bingo card
                 bingoNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25],
                 bingoPrize: [
